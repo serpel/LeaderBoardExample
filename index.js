b21493971145c77b7a4a1de8962f7abd3912b3b6
Sergio J. Peralta <sergio.peralta@herounit.io>
2020-10-23 11:00:50 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var sql = require('mssql');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser());
+
+var server = app.listen(8081, function () {
+    console.log("hola mundo estoy vivo!!!!");
+});
+
+app.get('/api/leaderboard', function(req, res){
+    console.log('lead endpoint');
+
+    var result = [
+            {
+                Username: 'sergio',
+                Points: 2000
+            },
+            {
+                Username: 'alex',
+                Points: 430
+            },
+            {
+                Username: 'javier',
+                Points: 100
+            }
+        ];
+
+    res.send(result); 
+});
+
+app.post('/api/v1/student', function(req, res, next){
+    console.log("Entro en post");
+    var name = req.body.name || "";
+    res.send(`El parametro mandado en el body es: ${name}`);
+});
